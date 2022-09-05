fbbbf687b4d533013abb1b4232f93c9f4be19e8b
santiago <davids.guerrero@udea.edu.co>
2022-09-05 13:02:05 -0500

    Initial commit

---
+++ src/main/java/app/retos/historicalreto/HistoricalRetoApplication.java
@@ -0,0 +1,13 @@
+package app.retos.historicalreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricalRetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/retos/historicalreto/HistoricalRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.retos.historicalreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricalRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
